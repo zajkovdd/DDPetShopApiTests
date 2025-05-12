1adde727f0b28ba014a6715fd9fbc8b9c17398b2
zajkovdd <zzigqushka@gmail.com>
2025-05-12 14:45:31 +0300

    Add tests 'Try to get info about non-existent pet' and 'Try to update non-existent pet' and refactor payload data for ID 31 and ID 32

--- tests/before_lesson_2.py
+++ tests/before_lesson_2.py
@@ -9,11 +9,13 @@ BASE_URL = 'http://5.181.109.28:9090/api/v3/'
 class TestPet:
     @allure.title('ID 31: Add new pet')
     def test_add_new_pet(self):
-        with allure.step('Add data for new pet'):
-            data = {"id": 1, "name": "Buddy", "status": "available"}
-
         with allure.step('Send request to add pet'):
-            response = requests.post(url=f'{BASE_URL}pet/', json=data)
+            payload = {
+                "id": 1,
+                "name": "Buddy",
+                "status": "available"
+            }
+            response = requests.post(url=f'{BASE_URL}pet/', json=payload)
 
         with allure.step('Check response code'):
             assert response.status_code == 200, 'Code is not correct with expected response'
@@ -23,11 +25,13 @@ class TestPet:
 
     @allure.title('ID 32: Get info about pet with ID')
     def test_get_info_about_pet_with_id(self):
-        with allure.step('Add data for new pet'):
-            data = {"id": 1, "name": "Buddy", "status": "available"}
-
         with allure.step('Send request to add pet'):
-            response = requests.post(url=f'{BASE_URL}pet/', json=data)
+            payload = {
+                "id": 1,
+                "name": "Buddy",
+                "status": "available"
+            }
+            response = requests.post(url=f'{BASE_URL}pet/', json=payload)
 
         with allure.step('Get pet ID from response'):
             petId = response.json().get('id')
@@ -41,3 +45,29 @@ class TestPet:
         with allure.step('Check text from response'):
             assert response.text == '{"id":1,"name":"Buddy","photoUrls":[],"tags":[],"status":"available"}', 'Text is not correct with expected response'
 
+    @allure.title('ID 37 Try to get info about non-existent pet')
+    def test_get_info_about_nonexistent_pet(self):
+        with allure.step('Send request to get info about non-existent pet'):
+            response = requests.get(url=f'{BASE_URL}pet/9999')
+
+        with allure.step('Check response code'):
+            assert response.status_code == 404, 'Code is not correct with expected response'
+
+        with allure.step('Check text from response'):
+            assert response.text == 'Pet not found', 'Text is not correct with expected response'
+
+    @allure.title('ID 38 Try to update non-existent pet')
+    def test_update_nonexistent_pet(self):
+        with allure.step('Send request to update non-existent pet'):
+            payload = {
+                "id": 9999,
+                "name": "Non-existent Pet",
+                "status": "available"
+            }
+            response = requests.put(url=f'{BASE_URL}pet', json=payload)
+
+        with allure.step('Check response code'):
+            assert response.status_code == 404, 'Code is not correct with expected response'
+
+        with allure.step('Check text from response'):
+            assert response.text == 'Pet not found', 'Text is not correct with expected response'
