1b1b10e2676f0ebb84494450e5b15c86bb4882d2
zajkovdd <zzigqushka@gmail.com>
2025-05-13 14:54:56 +0300

    Add .schemas in dir. Add pet_schema in .schemas. Add test 'Add new pet'

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,6 +1,7 @@
-
 import allure
+import jsonschema
 import requests
+from .schemas.pet_schema import PET_SCHEMA
 
 BASE_URL = 'http://5.181.109.28:9090/api/v3/'
 
@@ -43,4 +44,24 @@ class TestPet:
 
         with allure.step('Check text from response'):
             assert response.text == 'Pet not found', 'Text is not correct with expected response'
+
+    @allure.title('Add new pet')
+    def test_add_new_pet(self):
+        with allure.step('Send request to add pet'):
+            payload = {
+                "id": 1,
+                "name": "Buddy",
+                "status": "available"
+            }
+            response = requests.post(url=f'{BASE_URL}pet/', json=payload)
+            response_json = response.json()
+
+        with allure.step('Check response code and validate JSON_SCHEMA'):
+            assert response.status_code == 200, 'Code is not correct with expected response'
+            jsonschema.validate(response_json, PET_SCHEMA)
+
+        with allure.step('Check parameters of pet in response'):
+            assert response_json["id"] == payload["id"], 'Pet id is not correct with expected response'
+            assert response_json["name"] == payload["name"], 'Pet name is not correct with expected response'
+            assert response_json["status"] == payload["status"], 'Pet status is not correct with expected response'
 
